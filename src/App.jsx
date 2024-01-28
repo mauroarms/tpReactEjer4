ee910fbe5723f624c3934601087898f4c8230f38
mauroarms <mauro.armas14@gmail.com>
2024-01-28 13:59:03 -0300

    Se identifica y se gurda en arrayTarea las diferentes tareas

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import FormularioTarea from "./components/FormularioTarea";
 function App() {
   return (
     <>
-      <h1 className="display-1 text-center">Lista de Tareas</h1>
+      <h1 className="display-3 text-center">Lista de Tareas</h1>
 
       <Container className="mainPage d-flex flex-column align-items-center">      
 
         <section className=" d-flex flex-column border border-info-subtle border-3 rounded my-5 w-50 align-items-center ">
           <div className="text-center">
-            <h2 className="my-3 display-3">Bienvenido</h2>
+            <h2 className="my-3 display-5">Bienvenido</h2>
             <h4>Ingresa tus tareas</h4>
           </div>
           <div className="w-75 my-4">
--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,15 +1,34 @@
 import { Button, Form } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [arrayTarea, setArrayTarea] = useState([]);
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    setArrayTarea([...arrayTarea,tarea])
+    setTarea("")
+  }
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formularioTarea">
           <Form.Label>Titulo Tarea</Form.Label>
           <div className="d-flex gap-3">
-            <Form.Control type="text" placeholder="Mi Tarea" />
-            <Button className="btnAgregar" variant="outline-dark" type="submit">
+            <Form.Control
+              type="text"
+              placeholder="Mi Tarea"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <Button
+              className="btnAgregar"
+              variant="outline-dark"
+              type="submit"
+            >
               Agregar
             </Button>
           </div>
